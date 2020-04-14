94c6f9679f10edc13af8a874616c7d3572ec0a6b
Larry Lai <dppss91102@gmail.com>
2020-04-14 10:42:46 -0400

    Remove unnecassary space

--- src/test/java/com/csc510/smartweather/SmartweatherApplicationTests.java
+++ src/test/java/com/csc510/smartweather/SmartweatherApplicationTests.java
@@ -3,7 +3,6 @@ package com.csc510.smartweather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class SmartweatherApplicationTests {
 
