46abb9c67803edf8943a898d013500a734720ba4
LingLi <lli46@ncsu.edu>
2020-04-16 20:28:47 -0400

    get latitude and longitude

--- src/main/java/com/csc510/smartweather/controller/IndexController.java
+++ src/main/java/com/csc510/smartweather/controller/IndexController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import javax.servlet.ServletException;
-import java.io.IOException;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * @author Heng Yu
@@ -26,7 +27,24 @@ public class IndexController {
     @Autowired
     private QueryWeather queryWeather;
     @GetMapping("/")
-    public String index(HttpServletRequest request,Model model) {
+    public String index(Model model,
+                        HttpServletRequest request,
+                        @RequestParam(value = "latitude", required = false) String latitude,
+                        @RequestParam(value = "longitude", required = false) String longitude) {
+        //判断是否从前端收到经纬度，如果收到，标记session为已定位(located)
+        if (latitude != null && longitude != null) {
+            request.getSession().setAttribute("located", true);
+            System.out.println("纬度: " + latitude);
+            System.out.println("经度: " + longitude);
+        }
+
+        //检查当前session是否已定位，向前端传入flag，以确定是否调用javascript定位代码
+        Boolean located = (Boolean) request.getSession().getAttribute("located");
+        if (located != null && located) {
+            model.addAttribute("located",  true);
+        } else {
+            model.addAttribute("located", false);
+        }
 
         request.getSession().setAttribute("weatherID",queryWeather.CurrentWeatherInfo().getId());
         int weather_code = (Integer)request.getSession().getAttribute("weatherID");
